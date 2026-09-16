267036cc16278c4320e5b672b5c2d464f85dcbfc
Abdul Qadeer <2511-9fm-002@agu.edu.pk>
2026-09-16 03:40:20 -0700

    Create problem7.py

---
+++ problem7.py
@@ -0,0 +1,23 @@
+# get six number from the user
+# display 
+# clear list
+# then display
+
+lst = []
+# to get number from the user and store to list
+for i in range(6): # 0 1 2 3 4 5
+    lst.append(int(input("Enter 6 number")))
+
+print("This is the list")
+print(lst)
+lst.clear()
+print("We have removed the list items")
+print(lst)
+
+lst=[]
+for i in range(5):
+    lst.append(int(input("entr a namber:")))
+print(lst)
+lst.clear()
+print(lst)
+#
