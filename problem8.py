f267a40b0869148cf872327aeb244a09a068fbdc
Abdul Qadeer <2511-9fm-002@agu.edu.pk>
2026-09-16 04:58:33 -0700

    Create problem8.py

---
+++ problem8.py
@@ -0,0 +1,20 @@
+# To get a integer from the user
+# perform compound assignment operators
+
+number=int(input("enter a namber:"))
+#Addition
+number+=7
+print("After addition:",number)
+#substraction
+number-=8
+print("After substraction:",number)
+#multiplication
+number*=6
+print("After multiplication:",number)
+#division
+number/=6
+print("After division:",number)
+#modulus
+number%=8
+print("After modulus:",number)
+print("End")
