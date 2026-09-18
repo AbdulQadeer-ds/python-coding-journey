454ccfb8d909aa30a75d27adcd98b872185ed831
Abdul Qadeer <2511-9fm-002@agu.edu.pk>
2026-09-18 09:26:42 -0700

    updated

--- problem10.py
+++ problem10.py
@@ -4,5 +4,9 @@ for i in range(6,0,-1):
     for j in range(0,i):
         print("*",end=" ")
     print()
-
+#print star
+for i in range(0,6):
+    for j in range(0,i+1):
+        print("*",end="")
+    print()
     
--- problem9.py
+++ problem9.py
@@ -8,3 +8,9 @@ c = int(input("Enter c number"))
 
 result = (a+b+c)*(a/b)*(2*a+3*b)
 print("Result is = "+str(result))
+
+a=int(input("enter a namber"))
+b=int(input("enter a namber"))
+c=int(input("enter a namber"))
+result=(a+b+c)
+print(result)
